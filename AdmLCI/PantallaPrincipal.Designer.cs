da7f688cfa8b94ce184985c8761c395e23e9c18e
Jose Genaro <jose_28gena@live.com>
2013-09-29 17:52:55 -0700

    Etiqueta ariel

--- AdmLCI/PantallaPrincipal.Designer.cs
+++ AdmLCI/PantallaPrincipal.Designer.cs
@@ -608,9 +608,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(696, 73);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(69, 13);
             this.label2.TabIndex = 115;
-            this.label2.Text = "label2";
+            this.label2.Text = "Arieeeeeeel!!";
             // 
             // PantallaPrincipal
             // 
